0727c8d4f4cca51bd4b7b2994cc61e29dfffccb5
Your Name <you@example.com>
2021-12-29 21:56:03 +0700

    Fix to add ads admob

--- app/src/main/java/com/yay/gambar/FullScreenWallpaper.java
+++ app/src/main/java/com/yay/gambar/FullScreenWallpaper.java
@@ -1,5 +1,6 @@
 package com.yay.gambar;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.app.DownloadManager;
@@ -10,11 +11,21 @@ import android.graphics.Bitmap;
 import android.graphics.drawable.BitmapDrawable;
 import android.net.Uri;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Toast;
 
 import com.bumptech.glide.Glide;
 import com.github.chrisbanes.photoview.PhotoView;
+import com.google.android.gms.ads.AdError;
+import com.google.android.gms.ads.AdRequest;
+import com.google.android.gms.ads.FullScreenContentCallback;
+import com.google.android.gms.ads.LoadAdError;
+import com.google.android.gms.ads.MobileAds;
+import com.google.android.gms.ads.initialization.InitializationStatus;
+import com.google.android.gms.ads.initialization.OnInitializationCompleteListener;
+import com.google.android.gms.ads.interstitial.InterstitialAd;
+import com.google.android.gms.ads.interstitial.InterstitialAdLoadCallback;
 
 import java.io.IOException;
 
@@ -22,26 +33,110 @@ import yay.gambar.R;
 
 
 public class FullScreenWallpaper extends AppCompatActivity {
-    String originalUrl="";
+    String originalUrl = "";
     PhotoView photoView;
+
+    private InterstitialAd mInterstitialAd;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_full_screen_wallpaper);
 
         getSupportActionBar().hide();
-        Intent intent =  getIntent();
+        Intent intent = getIntent();
         originalUrl = intent.getStringExtra("originalUrl");
         photoView = findViewById(R.id.photoView);
         Glide.with(this).load(originalUrl).into(photoView);
 
 
+        MobileAds.initialize(this, new OnInitializationCompleteListener() {
+            @Override
+            public void onInitializationComplete(InitializationStatus initializationStatus) {
+            }
+        });
+        loadAd();
+
     }
 
-    public void SetWallpaperEvent(View view) {
 
+    public void loadAd() {
+        AdRequest adRequest = new AdRequest.Builder().build();
+
+        InterstitialAd.load(this, "ca-app-pub-3940256099942544/1033173712", adRequest,
+                new InterstitialAdLoadCallback() {
+                    @Override
+                    public void onAdLoaded(@NonNull InterstitialAd interstitialAd) {
+                        // The mInterstitialAd reference will be null until
+                        // an ad is loaded.
+                        mInterstitialAd = interstitialAd;
+
+                        mInterstitialAd.setFullScreenContentCallback(new FullScreenContentCallback() {
+                            @Override
+                            public void onAdFailedToShowFullScreenContent(@NonNull AdError adError) {
+                                super.onAdFailedToShowFullScreenContent(adError);
+                            }
+
+                            @Override
+                            public void onAdShowedFullScreenContent() {
+                                super.onAdShowedFullScreenContent();
+                            }
+
+                            @Override
+                            public void onAdDismissedFullScreenContent() {
+                                super.onAdDismissedFullScreenContent();
+                            }
+                        });
+                    }
+
+                    @Override
+                    public void onAdFailedToLoad(@NonNull LoadAdError loadAdError) {
+                        // Handle the error
+                        mInterstitialAd = null;
+                    }
+                });
+
+    }
+
+    private void reloadAd() {
+        if (mInterstitialAd == null) {
+            Log.e("TAG", "reloadAd: " );
+            loadAd();
+        }
+        Log.e("TAG", "reloadAd: 2" );
+    }
+
+
+    public void SetWallpaperEvent(View view) {
+        if (mInterstitialAd != null) {
+            mInterstitialAd.setFullScreenContentCallback(new FullScreenContentCallback() {
+                @Override
+                public void onAdFailedToShowFullScreenContent(@NonNull AdError adError) {
+                    super.onAdFailedToShowFullScreenContent(adError);
+                    mInterstitialAd = null;
+                    setWallpaper();
+                }
+
+
+                @Override
+                public void onAdDismissedFullScreenContent() {
+                    super.onAdDismissedFullScreenContent();
+                    mInterstitialAd = null;
+                    setWallpaper();
+
+                }
+            });
+            mInterstitialAd.show(this);
+        } else {
+            setWallpaper();
+        }
+    }
+
+    private void setWallpaper() {
+
+        reloadAd();
         WallpaperManager wallpaperManager = WallpaperManager.getInstance(this);
-        Bitmap bitmap  = ((BitmapDrawable)photoView.getDrawable()).getBitmap();
+        Bitmap bitmap = ((BitmapDrawable) photoView.getDrawable()).getBitmap();
         try {
             wallpaperManager.setBitmap(bitmap);
             Toast.makeText(this, "Wallpaper Set", Toast.LENGTH_SHORT).show();
@@ -53,11 +148,43 @@ public class FullScreenWallpaper extends AppCompatActivity {
 
     public void DownloadWallpaperEvent(View view) {
 
-        DownloadManager downloadManager = (DownloadManager)getSystemService(Context.DOWNLOAD_SERVICE);
+        if (mInterstitialAd != null) {
+            mInterstitialAd.setFullScreenContentCallback(new FullScreenContentCallback() {
+                @Override
+                public void onAdFailedToShowFullScreenContent(@NonNull AdError adError) {
+                    super.onAdFailedToShowFullScreenContent(adError);
+                    mInterstitialAd = null;
+                    downloadWall();
+                }
+
+                @Override
+                public void onAdShowedFullScreenContent() {
+                    super.onAdShowedFullScreenContent();
+                }
+
+                @Override
+                public void onAdDismissedFullScreenContent() {
+                    super.onAdDismissedFullScreenContent();
+                    mInterstitialAd = null;
+                    downloadWall();
+
+                }
+
+            });
+            mInterstitialAd.show(this);
+        } else {
+            downloadWall();
+        }
+    }
+
+    public void downloadWall() {
+        reloadAd();
+        DownloadManager downloadManager = (DownloadManager) getSystemService(Context.DOWNLOAD_SERVICE);
         Uri uri = Uri.parse(originalUrl);
         DownloadManager.Request request = new DownloadManager.Request(uri);
         request.setNotificationVisibility(DownloadManager.Request.VISIBILITY_VISIBLE_NOTIFY_COMPLETED);
         downloadManager.enqueue(request);
         Toast.makeText(this, "Downloading Start", Toast.LENGTH_SHORT).show();
+
     }
 }
--- app/src/main/java/com/yay/gambar/MainActivity.java
+++ app/src/main/java/com/yay/gambar/MainActivity.java
@@ -55,60 +55,13 @@ public class MainActivity extends AppCompatActivity {
     int currentItems,totalItems,scrollOutItems;
     String url ="https://api.pexels.com/v1/curated/?page="+pageNumber+"&per_page=80";
 
-    Button button;
-    private InterstitialAd mInterstitialAd;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        MobileAds.initialize(this, new OnInitializationCompleteListener() {
-            @Override
-            public void onInitializationComplete(InitializationStatus initializationStatus) {
-            }
-        });
-
-        AdRequest adRequest = new AdRequest.Builder().build();
-
-        InterstitialAd.load(this,"ca-app-pub-3940256099942544/1033173712", adRequest,
-                new InterstitialAdLoadCallback() {
-                    @Override
-                    public void onAdLoaded(@NonNull InterstitialAd interstitialAd) {
-                        // The mInterstitialAd reference will be null until
-                        // an ad is loaded.
-                        mInterstitialAd = interstitialAd;
-
-                        mInterstitialAd.setFullScreenContentCallback(new FullScreenContentCallback() {
-                            @Override
-                            public void onAdFailedToShowFullScreenContent(@NonNull AdError adError) {
-                                super.onAdFailedToShowFullScreenContent(adError);
-                            }
-
-                            @Override
-                            public void onAdShowedFullScreenContent() {
-                                super.onAdShowedFullScreenContent();
-                            }
-
-                            @Override
-                            public void onAdDismissedFullScreenContent() {
-                                super.onAdDismissedFullScreenContent();
-                            }
-                        });
-                    }
-
-                    @Override
-                    public void onAdFailedToLoad(@NonNull LoadAdError loadAdError) {
-                        // Handle the error
-                        mInterstitialAd = null;
-
-                        if (mInterstitialAd != null) {
-                            mInterstitialAd.show(MainActivity.this);
-                        } else {
-                            Log.d("TAG", "The interstitial ad wasn't ready yet.");
-                        }
-                    }
-                });
 
         recyclerView = findViewById(R.id.recyclerView);
         wallpaperModelList = new ArrayList<>();
@@ -147,21 +100,6 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
-        button = findViewById(R.id.btnSetWallpaper);
-        button.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-
-            }
-        });
-
-        button = findViewById(R.id.btnDownloadWallpaper);
-        button.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-
-            }
-        });
 
 
 
@@ -260,6 +198,7 @@ public class MainActivity extends AppCompatActivity {
                     String query = editText.getText().toString().toLowerCase();
 
                     url = "https://api.pexels.com/v1/search/?page="+pageNumber+"&per_page=80&query="+query;
+                    Log.e("TAG", "onClick: "+url );
                     wallpaperModelList.clear();
                     fetchWallpaper();
 
